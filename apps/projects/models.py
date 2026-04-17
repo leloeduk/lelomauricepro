3659c25714d6ed7312846417913f76cde1299df3
leloeduk <leloeduk2025@gmail.com>
2026-04-17 05:50:01 +0200

    feat: add projects app with model, views and dynamic UI

--- apps/projects/models.py
+++ apps/projects/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    tech_stack = models.CharField(max_length=255)
+    image = models.URLField(blank=True, null=True)
+    live_url = models.URLField(blank=True, null=True)
+    github_url = models.URLField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
--- apps/projects/views.py
+++ apps/projects/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Project
 
-# Create your views here.
+def project_list(request):
+    projects = Project.objects.all().order_by('-created_at')
+    return render(request, "projects/list.html", {"projects": projects})
